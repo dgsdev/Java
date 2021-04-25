f27c0f69f548dd5691de6e8c94aee80bbbe65ac4
Douglas Pinheiro <dgspramos@gmail.com>
2021-04-25 15:52:38 -0300

    print, println e quebra de linha

---
+++ Hello2.java
@@ -0,0 +1,10 @@
+// Print e \n múltipla linha na mesma instrução
+
+public class Hello2 {
+
+  public static void main(String[] args) {
+    System.out.print("Java -> ");
+    System.out.println("###  Hello New World! from ***DgsDev*** 2021  ###\n");
+    System.out.println(" Douglas\n Juliana\n Rebecca\n Programar\n Java");
+  }
+}
